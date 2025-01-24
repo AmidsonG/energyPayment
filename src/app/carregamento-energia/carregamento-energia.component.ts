b616e2db43bd7e062ea87ae07e7e2853791a14e8
AmidsonG <amidson10@gmail.com>
2025-01-24 16:32:52 +0100

    mensagem de erro

--- src/app/carregamento-energia/carregamento-energia.component.ts
+++ src/app/carregamento-energia/carregamento-energia.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CarregarEnergiaService } from '../services/carregar-energia.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-carregamento-energia',
   templateUrl: './carregamento-energia.component.html',
@@ -10,6 +10,8 @@ export class CarregamentoEnergiaComponent implements OnInit {
   userData: any;
   carregaEnergia?: number ;
   recargas: any;
+  CameraAdicionado: boolean = false;
+  erroAoAdicionarCamera: boolean = false;
 
   constructor( private carregarEnergiaService: CarregarEnergiaService) {
     const user = localStorage.getItem('user');
@@ -27,7 +29,19 @@ export class CarregamentoEnergiaComponent implements OnInit {
       id_do_usuario: this.userData.id,
       codigo_da_recarga: this.carregaEnergia,
     }
-    this.carregarEnergiaService.carregarEnergia(params).subscribe();
+    this.carregarEnergiaService.carregarEnergia(params).subscribe(
+      (response) => {
+        Swal.fire('Sucesso', 'Carregado com Sucesso', 'success');
+        this.CameraAdicionado = true;
+          this.erroAoAdicionarCamera = false;
+      },
+      (error) => {
+        Swal.fire('Erro', 'Digite o apenas números', 'error');
+        this.CameraAdicionado = false;
+        this.erroAoAdicionarCamera = true;
+      }
+
+    );
   }
 
   getTotalRecargas(){
